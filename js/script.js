3927ca777452ba742a3105ccc57dd51264249228
22007712 <22007712@uhi.ac.uk>
2025-04-21 18:31:32 +0000

    modify

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@ function expandNavbar() {
     var element = document.getElementById("navbar");
     element.classList.toggle("navbar-expanded");
 }
+
+var cursor = document.querySelector(".circle")
+
+function getDimensions(e) {
+  cursor.style.top = e.pageY + "px" // -25px for the size of the circle
+  cursor.style.left = e.pageX + "px"
+}
+
+window.addEventListener("mousemove", (e) => {
+  getDimensions(e)
+});
+
+document.querySelectorAll("img").forEach(function(img) {
+    img.addEventListener("mouseenter", function() {
+        cursor.classList.add("expanded");
+    });
+    img.addEventListener("mouseleave", function() {
+        cursor.classList.remove("expanded");
+    });
+  });
+
+  document.querySelectorAll("a").forEach(function(a) {
+    a.addEventListener("mouseenter", function() {
+        cursor.classList.add("expanded");
+    });
+    a.addEventListener("mouseleave", function() {
+        cursor.classList.remove("expanded");
+    });
+  });
